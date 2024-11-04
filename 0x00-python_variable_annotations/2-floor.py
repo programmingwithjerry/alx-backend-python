0fb2629a1596f8df43e76eb0fbbcf87d854b4791
programmingwithjerry <eneyellowj@gmail.com>
2024-11-04 22:14:23 +0100

    2-floor.py

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -4,8 +4,8 @@ This module contains a type-annotated function `floor`
 that returns the floor of a float.
 """
 
-
 import math
+
 
 def floor(n: float) -> int:
     """Returns the floor of the given float."""
