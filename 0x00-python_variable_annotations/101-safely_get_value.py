a8be6f6b7ccbfc53b30ce9b85b0e2c3d4d9f0772
programmingwithjerry <eneyellowj@gmail.com>
2024-11-04 23:25:10 +0100

    101-safely_get_value.py

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """
-Type-annotated function that safely retrieves a value from a dictionary.
+Type-annotated function to safely fetch a value from a dictionary.
 """
-from typing import Any, TypeVar, Mapping, Optional
+from typing import Any, Mapping, Union, TypeVar
 
 T = TypeVar('T')
+Res = Union[Any, T]
+Def = Union[T, None]
 
-def safely_get_value(dct: Mapping[Any, T], key: Any, default: Optional[T] = None) -> Optional[T]:
-    """Safely gets a value from a dictionary using the provided key; returns
-       the default if the key is not found."""
+def safely_get_value(dct: Mapping, key: Any, default: Def = None) -> Res:
+    """Fetches a value from a dictionary using a given key; returns
+       a default value if the key is not present."""
     if key in dct:
         return dct[key]
     else:
