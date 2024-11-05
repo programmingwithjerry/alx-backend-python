c72379f4cb82095803eeeeb3617026c426c6e58c
programmingwithjerry <eneyellowj@gmail.com>
2024-11-05 11:14:40 +0100

    1-concurrent_coroutines.py

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
+"""Asynchronous Python function that gathers and
+   sorts the results of multiple coroutines.
 """
-Asynchronous Python function that gathers and
-sorts the results of multiple coroutines.
-"""
-
 import asyncio
 from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a sorted list of delays collected from
