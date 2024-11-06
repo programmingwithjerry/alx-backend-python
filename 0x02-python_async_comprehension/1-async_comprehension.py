7d06760ca89ef66116bb2b983ebe3587589802f2
programmingwithjerry <eneyellowj@gmail.com>
2024-11-06 15:07:00 +0100

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,8 +3,8 @@
 A module containing a coroutine called async_comprehension that collects
 10 random numbers using async comprehensions over async_generator.
 """
-import typing
 
+import typing
 
 # Importing async_generator from the previous task
 async_generator = __import__('0-async_generator').async_generator
